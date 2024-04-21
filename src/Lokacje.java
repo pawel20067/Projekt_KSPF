ecdb75c5ae49b8ae01cde391b54e05ec357d7204
pawel20067 <pawelsoty@gmail.com>
2024-04-21 14:05:28 +0200

    dodanie funkcjonalności poruszania się

--- src/Lokacje.java
+++ src/Lokacje.java
@@ -1,4 +1,58 @@
+class Lokacje {
+    protected String nazwa;
+    protected String opis;
+    protected Lokacje[] przejscia = new Lokacje[4];
 
-public class Lokacje {
+    public Lokacje(String nazwa) {
+        this.nazwa = nazwa;
+    }
 
+    public String wypiszPrzejscia() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < przejscia.length; i++) {
+            if (przejscia[i] != null) {
+                if (builder.length() > 0) {
+                    builder.append(", ");
+                }
+                switch(i) {
+                    case 0:
+                        builder.append("N (" + przejscia[i].getNazwa() + ")");
+                        break;
+                    case 1:
+                        builder.append("S (" + przejscia[i].getNazwa() + ")");
+                        break;
+                    case 2:
+                        builder.append("E (" + przejscia[i].getNazwa() + ")");
+                        break;
+                    case 3:
+                        builder.append("W (" + przejscia[i].getNazwa() + ")");
+                        break;
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public void dodajPrzejscie(Lokacje n, Lokacje p, Lokacje t, Lokacje l) {
+        przejscia[0] = n;
+        przejscia[1] = p;
+        przejscia[2] = t;
+        przejscia[3] = l;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,81 @@
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Czesc chlopaki");
-		System.out.println("Czesc chlopaku");
-		System.out.println("test test test");
-		System.out.println("Elo ELo");
-	}
+    public static void main(String[] args) {
+        Lokacje[] swiat = new Lokacje[25];
 
+        swiat[0] = new Lokacje("Polska: Klucze");
+        swiat[1] = new Lokacje("Polska: Katowice");
+        swiat[2] = new Lokacje("Słowacja: Skalite");
+        swiat[3] = new Lokacje("Węgry: Hant");
+        swiat[4] = new Lokacje("Czechy: Karniów");
+        swiat[5] = new Lokacje("Austra: Kleinhaudorf");
+        swiat[6] = new Lokacje("Niemcy: Schiroliry");
+        swiat[7] = new Lokacje("Francja: Strasburg");
+        swiat[8] = new Lokacje("Hiszpania: Ruigcerda");
+        swiat[9] = new Lokacje("Portugalia: Ayanonte");
+
+        swiat[0].dodajPrzejscie(null, null, null, swiat[1]);
+        swiat[1].dodajPrzejscie(null, swiat[0], swiat[2], swiat[4]);
+        swiat[2].dodajPrzejscie(swiat[1], null, swiat[3], swiat[5]);
+        swiat[3].dodajPrzejscie(swiat[2], null, null, null);
+        swiat[4].dodajPrzejscie(null, swiat[1], swiat[5], swiat[6]);
+        swiat[5].dodajPrzejscie(swiat[4], swiat[2], null, null);
+        swiat[6].dodajPrzejscie(null, swiat[4], null, swiat[7]);
+        swiat[7].dodajPrzejscie(null, swiat[6], swiat[8], null);
+        swiat[8].dodajPrzejscie(swiat[7], null, null, swiat[9]);
+        swiat[9].dodajPrzejscie(null, swiat[8], null, null);
+
+        Lokacje aktualneMiejsce = swiat[0];
+        Scanner scan = new Scanner(System.in);
+        
+        int wygrana = 0;
+
+        while (wygrana!=1) {
+            System.out.println("Znajdujesz się w " + aktualneMiejsce.getNazwa());
+            System.out.println("Możesz pojechać do:");
+            System.out.println("");
+
+            String opcje = aktualneMiejsce.wypiszPrzejscia();
+            if (opcje.isEmpty()) {
+                System.out.println("Brak dostępnych miejsc do odwiedzenia.");
+            } else {
+                System.out.println(opcje);
+            }
+            
+            System.out.println("");
+
+            System.out.print("Wpisz kierunek geograficzny (N, S, E, W): ");
+            String wybor = scan.nextLine().toUpperCase();
+
+            Lokacje noweMiejsce = null;
+            switch (wybor) {
+                case "N":
+                    noweMiejsce = aktualneMiejsce.przejscia[0];
+                    break;
+                case "S":
+                    noweMiejsce = aktualneMiejsce.przejscia[1];
+                    break;
+                case "E":
+                    noweMiejsce = aktualneMiejsce.przejscia[2];
+                    break;
+                case "W":
+                    noweMiejsce = aktualneMiejsce.przejscia[3];
+                    break;
+                case "Q":
+                    System.out.println("Zakończono podróż.");
+                    return;
+                default:
+                    System.out.println("Nie możesz się tam wybrać. Wybierz dośtępną opcje.");
+                    continue;
+            }
+
+            if (noweMiejsce != null) {
+                aktualneMiejsce = noweMiejsce;
+            } else {
+                System.out.println("Nie możesz się tam wybrać. Wybierz dośtępną opcje.");
+            }
+        }
+    }
 }
